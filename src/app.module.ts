1ade232947e661a78f83e217148e0ffbb9ac608d
mohamedamin12 <aboamin909080@gmail.com>
2025-04-02 18:50:06 +0200

    configure TypeORM with PostgreSQL and add development environment support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: "postgres",
+          database: config.get<string>("DB_DATABASE"),
+          username: config.get<string>("DB_USERNAME"),
+          password: config.get<string>("DB_PASSWORD"),
+          port: config.get<number>("DB_PORT"),
+          host: 'localhost',
+          synchronize: process.env.NODE_ENV !== 'production',
+          // entities: []
+        };
+      }
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env.development"
+      // envFilePath: process.env.NODE_ENV !== 'production' ? `.env.${process.env.NODE_ENV}` : ".env"
+    }),
+  ],
 
 })
 export class AppModule {}
